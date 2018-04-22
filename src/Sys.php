3f7303dd787576ff5f5c60cbf7075bdca9e555a5
inhere <in.798@qq.com>
2018-04-22 13:09:49 +0800

    Squashed 'libs/sys-utils/' content from commit 54327e5
    git-subtree-dir: libs/sys-utils
    git-subtree-split: 54327e5a04fe784a3652d6931fb756c8cda0ef6a

---
+++ src/Sys.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inhere
+ * Date: 2017/6/27
+ * Time: 下午8:18
+ */
+
+namespace MyLib\SysUtil;
+
+/**
+ * Class Sys
+ * @package MyLib\SysUtil
+ */
+class Sys extends SysEnv
+{
+    /**
+     * run a command in background
+     * @param string $cmd
+     */
+    public static function execInBackground($cmd)
+    {
+        if (self::isWindows()) {
+            pclose(popen('start /B ' . $cmd, 'r'));
+        } else {
+            exec($cmd . ' > /dev/null &');
+        }
+    }
+
+    /**
+     * @param string $command
+     * @param null|string $logfile
+     * @param null|string $user
+     * @return mixed
+     * @throws \RuntimeException
+     */
+    public static function exec($command, $logfile = null, $user = null)
+    {
+        // If should run as another user, we must be on *nix and must have sudo privileges.
+        $suDo = '';
+
+        if ($user && self::isUnix() && self::isRoot()) {
+            $suDo = "sudo -u $user";
+        }
+
+        // Start execution. Run in foreground (will block).
+        $logfile = $logfile ?: self::getNullDevice();
+
+        // Start execution. Run in foreground (will block).
+        exec("$suDo $command 1>> \"$logfile\" 2>&1", $dummy, $retVal);
+
+        if ($retVal !== 0) {
+            throw new \RuntimeException("command exited with status '$retVal'.");
+        }
+
+        return $dummy;
+    }
+
+    /**
+     * Method to execute a command in the sys
+     * Uses :
+     * 1. system
+     * 2. passthru
+     * 3. exec
+     * 4. shell_exec
+     * @param $command
+     * @param bool $returnStatus
+     * @return array
+     */
+    public static function runCommand($command, $returnStatus = true)
+    {
+        $return_var = 1;
+
+        //system
+        if (\function_exists('system')) {
+            ob_start();
+            system($command, $return_var);
+            $output = ob_get_contents();
+            ob_end_clean();
+
+            // passthru
+        } elseif (\function_exists('passthru')) {
+            ob_start();
+            passthru($command, $return_var);
+            $output = ob_get_contents();
+            ob_end_clean();
+            //exec
+        } else {
+            if (\function_exists('exec')) {
+                exec($command, $output, $return_var);
+                $output = implode("\n", $output);
+
+                //shell_exec
+            } else {
+                if (\function_exists('shell_exec')) {
+                    $output = shell_exec($command);
+                } else {
+                    $output = 'Command execution not possible on this system';
+                    $return_var = 0;
+                }
+            }
+        }
+
+        if ($returnStatus) {
+            return ['output' => trim($output), 'status' => $return_var];
+        }
+
+        return trim($output);
+    }
+
+    /**
+     * @return string
+     */
+    public static function getTempDir(): string
+    {
+        // @codeCoverageIgnoreStart
+        if (\function_exists('sys_get_temp_dir')) {
+            $tmp = sys_get_temp_dir();
+        } elseif (!empty($_SERVER['TMP'])) {
+            $tmp = $_SERVER['TMP'];
+        } elseif (!empty($_SERVER['TEMP'])) {
+            $tmp = $_SERVER['TEMP'];
+        } elseif (!empty($_SERVER['TMPDIR'])) {
+            $tmp = $_SERVER['TMPDIR'];
+        } else {
+            $tmp = getcwd();
+        }
+        // @codeCoverageIgnoreEnd
+
+        return $tmp;
+    }
+
+    /**
+     * @param string $program
+     * @return int|string
+     */
+    public static function getCpuUsage($program)
+    {
+        if (!$program) {
+            return -1;
+        }
+
+        $info = exec('ps aux | grep ' . $program . ' | grep -v grep | grep -v su | awk {"print $3"}');
+
+        return $info;
+    }
+
+    /**
+     * @param $program
+     * @return int|string
+     */
+    public static function getMemUsage($program)
+    {
+        if (!$program) {
+            return -1;
+        }
+
+        $info = exec('ps aux | grep ' . $program . ' | grep -v grep | grep -v su | awk {"print $4"}');
+
+        return $info;
+    }
+
+
+    /**
+     * 支持查看指定目录，默认当前目录
+     * CLI:
+     *     php test.php -d=path
+     *     php test.php --dir=path
+     * WEB:
+     *    /test.php?dir=path
+     */
+    public static function gitCheck()
+    {
+        if (PHP_SAPI === 'cli') {
+            $_GET = getopt('d::', ['dir::']);
+        }
+
+        // 获取要查看的目录，没有则检测当前目录
+        $dir = $_GET['d'] ?? ($_GET['dir'] ?? __DIR__);
+
+        if (!is_dir($dir)) {
+            trigger_error($dir);
+        }
+
+        ob_start();
+        system("cd $dir && git branch -v");
+        $c = ob_get_clean();
+
+        $result = preg_match('#\* (?<brName>[\S]+)(?:\s+)(?<logNum>[0-9a-z]{7})(?<ciText>.*)#i', $c, $data);
+        $data['projectName'] = basename($dir);
+
+        // var_dump($c,$result, $data);
+        return ($result === 1) ? $data : null;
+    }
+}
---
+++ src/SysEnv.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inhere
+ * Date: 2017/6/9
+ * Time: 下午10:06
+ */
+
+namespace MyLib\SysUtil;
+
+/**
+ * Class EnvHelper
+ * @package MyLib\SysUtil
+ */
+class SysEnv
+{
+    /**************************************************************************
+     * system env
+     *************************************************************************/
+
+    /**
+     * @return bool
+     */
+    public static function isUnix(): bool
+    {
+        $uNames = ['CYG', 'DAR', 'FRE', 'HP-', 'IRI', 'LIN', 'NET', 'OPE', 'SUN', 'UNI'];
+
+        return \in_array(strtoupper(substr(PHP_OS, 0, 3)), $uNames, true);
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isLinux(): bool
+    {
+        return stripos(PHP_OS, 'LIN') !== false;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isWin(): bool
+    {
+        return self::isWindows();
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isWindows(): bool
+    {
+        return stripos(PHP_OS, 'WIN') !== false;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isMac(): bool
+    {
+        return stripos(PHP_OS, 'Darwin') !== false;
+    }
+
+    /**
+     * @return bool
+     */
+    public static function isRoot(): bool
+    {
+        if (\function_exists('posix_getuid')) {
+            return posix_getuid() === 0;
+        }
+
+        return getmyuid() === 0;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getHostname()
+    {
+        return php_uname('n');
+    }
+
+    /**
+     * @return string
+     */
+    public static function getNullDevice()
+    {
+        if (self::isUnix()) {
+            return '/dev/null';
+        }
+
+        return 'NUL';
+    }
+
+    /**
+     * Returns true if STDOUT supports colorization.
+     * This code has been copied and adapted from
+     * \Symfony\Component\Console\Output\OutputStream.
+     * @return boolean
+     */
+    public static function isSupportColor()
+    {
+        if (DIRECTORY_SEPARATOR === '\\') {
+            return
+                '10.0.10586' === PHP_WINDOWS_VERSION_MAJOR . '.' . PHP_WINDOWS_VERSION_MINOR . '.' . PHP_WINDOWS_VERSION_BUILD
+                || false !== getenv('ANSICON')
+                || 'ON' === getenv('ConEmuANSI')
+                || 'xterm' === getenv('TERM')// || 'cygwin' === getenv('TERM')
+                ;
+        }
+
+        if (!\defined('STDOUT')) {
+            return false;
+        }
+
+        return self::isInteractive(STDOUT);
+    }
+
+    /**
+     * Returns if the file descriptor is an interactive terminal or not.
+     * @param  int|resource $fileDescriptor
+     * @return boolean
+     */
+    public static function isInteractive($fileDescriptor)
+    {
+        return \function_exists('posix_isatty') && @posix_isatty($fileDescriptor);
+    }
+
+}
