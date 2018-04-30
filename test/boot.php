2f0a3a29fa004f0fc74e866adfbf75b7b14d0464
inhere <in.798@qq.com>
2018-04-30 15:46:15 +0800

    Squashed 'libs/sys-utils/' changes from 9b0ad81..b21c7e9
    b21c7e9 Merge commit '0f2d5c1633e242102d161ad2e83d61e06b1d3e3d'
    d36e174 add test config. update sys-utils namespace
    
    git-subtree-dir: libs/sys-utils
    git-subtree-split: b21c7e9743f8ab52e43afe7dc4d9553aa2b77b95

--- test/boot.php
+++ test/boot.php
@@ -10,14 +10,14 @@ date_default_timezone_set('Asia/Shanghai');
 spl_autoload_register(function ($class) {
     $file = null;
 
-    if (0 === strpos($class,'Toolkit\SysUtil\Example\\')) {
-        $path = str_replace('\\', '/', substr($class, strlen('Toolkit\SysUtil\Example\\')));
+    if (0 === strpos($class,'Toolkit\Sys\Example\\')) {
+        $path = str_replace('\\', '/', substr($class, strlen('Toolkit\Sys\Example\\')));
         $file = dirname(__DIR__) . "/example/{$path}.php";
-    } elseif (0 === strpos($class,'Toolkit\SysUtil\Test\\')) {
-        $path = str_replace('\\', '/', substr($class, strlen('Toolkit\SysUtil\Test\\')));
+    } elseif (0 === strpos($class,'Toolkit\Sys\Test\\')) {
+        $path = str_replace('\\', '/', substr($class, strlen('Toolkit\Sys\Test\\')));
         $file = __DIR__ . "/{$path}.php";
-    } elseif (0 === strpos($class,'Toolkit\SysUtil\\')) {
-        $path = str_replace('\\', '/', substr($class, strlen('Toolkit\SysUtil\\')));
+    } elseif (0 === strpos($class,'Toolkit\Sys\\')) {
+        $path = str_replace('\\', '/', substr($class, strlen('Toolkit\Sys\\')));
         $file = dirname(__DIR__) . "/src/{$path}.php";
     }
 
